207217d565746924ae182e51f19ba905bf7e63e1
MrChrister <christer.sund@netikka.fi>
2013-02-18 02:19:21 -0800

    insert to database

--- app/controllers/AdminController.php
+++ app/controllers/AdminController.php
@@ -14,5 +14,36 @@ class AdminController extends Zend_Controller_Action
 	{
 		$this->view->test = "Controller output";
 	}
+	
+	public function projectAction()
+	{
+		
+		$request = $this->getRequest();
+		$mode = $request->getParam('mode');
+		$form = new Edulab_Form_Add();
+		
+		if($mode == "new")
+		{
+			$this->view->form=$form;
+		}
+		elseif($mode == "save")
+		{
+			if($this->getRequest()->isPost())
+			{
+				$formData = $this->_request->getPost();
+				if($form->isValid($formData))
+				{
+				$title = $form->getValue('title');
+				$description = $form->getValue('description');
+				$programmecode = $form->getValue('programmecode');
+				$projects = new Edulab_Model_Project();
+				$projects->addProjects($title,$description,$programmecode);
+				}
+			}
+			
+			$this->_redirect('admin/project/mode/new');
+		}
+	}
+
 }
 ?>
